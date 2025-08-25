8c9e7d4421c376b6f5252f9463e794817055d949
Christopher Conlin <cconlin@health.ucsd.edu>
2025-08-25 11:51:28 -0700

    Updated rules for selecting relevant series from whole MRI exam

--- fetch_RSI_paths.m
+++ fetch_RSI_paths.m
@@ -8,7 +8,6 @@ paths.RSI = {''};
 
 % Optional
 paths.T2_ax = {''};
-paths.T2_sag_cube = {''};
 paths.DWI_conventional = {''};
 paths.ADC_conventional = {''};
 paths.DCE = {''};
@@ -22,26 +21,23 @@ paths.Dixon_water = {''};
 % Ignore
 global_exclude = {'.', '..', '.DS_Store'};
 
+% RSI general
+patterns_RSI = {'RSI', 'b0b100b800b1400b2500'};
+patterns_exclude_RSI = {'trace', 'adc', 'color', 'fa', 'tensor'};
+
 % RSI GE 
 seqID_RSI_GE = {'epi2_pepolarFOCUSFLEX', 'epi2_pepolarFLEX', 'epi2_ART', 'epi2_revART', 'epi2alt', 'epi2altoff'};
-patterns_exclude_RSI = {'trace', 'adc', 'color'};
 
 % RSI Siemens
 patterns_seqID_RSI_Siemens = {'ep_b', 'ez_b', 'WI_b'};
-patterns_exclude_RSI_siemens = {'trace', 'adc', 'color', 'fa', 'tensor'};
 
 % RSI Philips
 patterns_seqID_RSI_philips = {'DwiSE'};
-patterns_exclude_RSI_philips = {'trace', 'adc', 'color', 'fa', 'tensor'};
 
 % Axial T2 for RSI overlay
 patterns_T2_ax_plane = {'ax', 'tra'};
 patterns_T2_ax_contrast = {'T2', 'FSE'};
 patterns_exclude_T2_ax = {'water', 'fat', 'flex', 'sag', 'cor', 'reformat', 'cube', 'T1', 'bifurcation', 'prop', '3d', 'space'};
-
-% Sag Cube
-seqID_cube_GE = 'Cube';
-patterns_T2_sag_cube = 'Sag';
 
 % Conventional DWI
 seqID_DWI_GE = 'epi2';
@@ -125,11 +121,10 @@ for i = 1:length(acqs)
       seq_name = info.MRSeriesScanningTechniqueDesc;
     end
 
-
     % Check for RSI data
     if strcmp(manufacturer, 'ge')
       match_RSI_seq = any(~cellfun(@isempty, regexpi(seq_name, seqID_RSI_GE)));
-      match_RSI_name = ~isempty(regexpi(SeriesDescription, 'RSI'));
+      match_RSI_name = any(~cellfun(@isempty, regexpi(SeriesDescription, patterns_RSI)));
       match_exclude = any(~cellfun(@isempty, regexpi(SeriesDescription, patterns_exclude_RSI)));
       if (match_RSI_seq || match_RSI_name) && ~match_exclude
 	paths.RSI{RSI_path_num} = acq_path;
@@ -139,7 +134,7 @@ for i = 1:length(acqs)
     end
     if strcmp(manufacturer, 'siemens')
       match_RSI_seq = any(~cellfun(@isempty, regexpi(seq_name, patterns_seqID_RSI_Siemens)));
-      match_exclude = any(~cellfun(@isempty, regexpi(SeriesDescription, patterns_exclude_RSI_siemens)));
+      match_exclude = any(~cellfun(@isempty, regexpi(SeriesDescription, patterns_exclude_RSI)));
       if match_RSI_seq && ~match_exclude
 	paths.RSI{RSI_path_num} = acq_path;
 	series_description_list{RSI_path_num} = SeriesDescription; % Save for later filtering of reverse acquisitions
@@ -148,7 +143,7 @@ for i = 1:length(acqs)
     end
     if strcmp(manufacturer, 'philips')
       match_RSI_seq = any(~cellfun(@isempty, regexpi(seq_name, patterns_seqID_RSI_philips)));
-      match_RSI_name = ~isempty(regexpi(SeriesDescription, 'b0b100b800b1400b2500'));
+      match_RSI_name = any(~cellfun(@isempty, regexpi(SeriesDescription, patterns_RSI)));
       match_exclude = any(~cellfun(@isempty, regexpi(SeriesDescription, patterns_exclude_RSI)));
       if (match_RSI_seq && match_RSI_name) && ~match_exclude
 	paths.RSI{RSI_path_num} = acq_path;
@@ -165,12 +160,6 @@ for i = 1:length(acqs)
     if match_T2_plane && match_T2_contrast && ~match_exclude && ~match_exclude_reformat
       paths.T2_ax{T2_ax_path_num} = acq_path;
       T2_ax_path_num = T2_ax_path_num + 1;
-    end
-
-    % Check for sag T2 cube
-    if strcmp(seq_name, seqID_cube_GE) && ~isempty(regexpi(SeriesDescription, patterns_T2_sag_cube))
-       paths.T2_sag_cube{T2_sag_cube_path_num} = acq_path;
-       T2_sag_cube_path_num = T2_sag_cube_path_num + 1;
     end
 
     % Check for conventional DWI
@@ -227,15 +216,23 @@ end
 
 % Separate reverse PE volumes, if any
 paths.RSI_rev = cell(size(paths.RSI));
+
 for i = 1:length(paths.RSI)
+
   series1 = series_description_list{i};
+  pattern_rev = sprintf('%s.*rev', series1);
+
   for j = 1:length(paths.RSI)
+
     series2 = series_description_list{j};
-    if strcmp(series2, [series1 '_rev'])
+
+    if ~isempty(regexpi(series2, pattern_rev))
       paths.RSI_rev{i} = paths.RSI{j};
       paths.RSI{j} = [];
     end
+
   end
+
 end
 
 
